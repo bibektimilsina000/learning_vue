1a487074481091f2cccb23e61385487c35b8edca
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-12 12:33:56 +0545

    data method computed watch

--- DomUnderstanding/app.js
+++ DomUnderstanding/app.js
@@ -1,22 +1,63 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-      name: ''
-    };
-  },
-  methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    data() {
+        return {
+            counter: 0,
+            name: '',
+            fullname: '',
+
+        }
     },
-    add(num) {
-      this.counter = this.counter + num;
+    watch: {
+
+        name(value) {
+
+            if (value === '') {
+                this.fullname = '';
+            } else {
+                this.fullname = value + ' ' + 'Panta'
+            }
+
+        }
+
     },
-    reduce(num) {
-      this.counter = this.counter - num;
-      // this.counter--;
-    }
-  }
+
+    computed: {
+        // fullname() {
+        //     console.log('running again...')
+
+        //     if (this.name === '') {
+        //         return '';
+        //     }
+
+        //     return this.name + ' ' + 'Panta';
+        // },
+
+
+
+    },
+    methods: {
+
+        outputFullName() {
+            console.log('running again...')
+
+            if (this.name === '') {
+                return ''
+            }
+
+            return this.name + ' ' + 'Panta'
+        },
+
+        add() {
+            this.counter = this.counter + 5
+        },
+        reset() {
+
+            this.name = '';
+
+        }
+
+
+    },
 });
 
-app.mount('#events');
+app.mount('#events')
