2e9e45e39725dcf635359cf12c650ad76d49655e
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-12 13:43:28 +0545

    dynamic styling section ends

---
+++ dynamyc styling/basics-10-styling-starting-setup/app.js
@@ -0,0 +1,47 @@
+const app = Vue.createApp({
+    data() {
+
+        return {
+            boxAselected: false,
+            boxBselected: false,
+            boxCselected: false,
+        }
+    },
+    computed:{
+        
+         boxAclasses(){
+            return { active:this.boxAselected};
+         }
+    },
+
+    methods: {
+        selectBox(box) {
+
+            if (box === "A") {
+
+
+                this.boxAselected = !this.boxAselected;
+                 
+            }
+            else if (box === "B") {
+                 
+                this.boxBselected = !this.boxBselected;
+                
+            }
+            else if (box === "C") {
+                 
+                 
+                this.boxCselected = !this.boxCselected;
+            }
+        }
+    }
+
+
+
+
+
+
+
+})
+
+app.mount('#styling')
