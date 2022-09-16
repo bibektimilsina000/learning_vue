9a9d0db9c95b542a06cfd4ca5f77b4fac1ebe862
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-17 02:32:57 +0545

    The end

--- monstergame/app.js
+++ monstergame/app.js
@@ -13,14 +13,21 @@ const app = Vue.createApp({
       currentRound: 0,
       disableHeal: false,
       winner: null,
+      logmsgs: [],
     };
   },
 
   computed: {
     monsterBarStyle() {
+      if (this.monsterHealth <= 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyle() {
+      if (this.playerHealth <= 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
     },
     useSpecialAttack() {
@@ -50,22 +57,32 @@ const app = Vue.createApp({
   },
 
   methods: {
+    restartGame() {
+      (this.playerHealth = 100),
+        (this.monsterHealth = 100),
+        (this.counter = 0),
+        (this.currentRound = 0);
+      (this.disableHeal = false), (this.winner = null), (this.logmsgs = []);
+    },
     attackMonster() {
       this.currentRound++;
-      const randomvalue = getRandomValue(5, 12);
+      const randomvalue = getRandomValue(5, 15);
 
       this.monsterHealth -= randomvalue;
       this.attackPlayer();
+      this.battellog("player", "attack", randomvalue);
     },
     attackPlayer() {
       const randomvalue = getRandomValue(8, 18);
       this.playerHealth -= randomvalue;
+      this.battellog("monster", "attack", randomvalue);
     },
     specialAttack() {
       this.counter++;
       const randomvalue = getRandomValue(10, 25);
       this.monsterHealth -= randomvalue;
       this.attackPlayer();
+      this.battellog("player", "attack", randomvalue);
     },
     heal() {
       const randomvalue = getRandomValue(15, 20);
@@ -75,6 +92,17 @@ const app = Vue.createApp({
         this.playerHealth += randomvalue;
       }
       this.disableHeal = true;
+      this.battellog("player", "heal", randomvalue);
+    },
+    surrender() {
+      this.winner = "monster";
+    },
+    battellog(who, what, value) {
+      this.logmsgs.unshift({
+        actionby: who,
+        actiontype: what,
+        actionvalue: value,
+      });
     },
   },
 });
